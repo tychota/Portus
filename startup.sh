4e897ac4d481d38d588c3c7202dd42893f17f6d1
tychota <tychota@users.noreply.github.com>
2016-07-11 20:56:59 +0200

    Improve security

--- startup.sh
+++ startup.sh
@@ -14,10 +14,46 @@ if [ "$PORTUS_MACHINE_FQDN" = "" ]; then
 fi
 mkdir -p /etc/nginx/conf.d
 cat >/etc/nginx/conf.d/portus.conf <<_END_
-      server {
-        listen 443 ssl;
-        ssl_certificate     certs/$NAME.crt;
-        ssl_certificate_key certs/$NAME.key;
+    server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        # Redirect all HTTP requests to HTTPS with a 301 Moved Permanently response.
+        return 301 https://$host$request_uri;
+    }
+    server {
+        listen                  443 default_server ssl  http2;
+
+        ssl_certificate         certs/$NAME.crt;
+        ssl_certificate_key     certs/$NAME.key;
+        ssl_session_timeout     1d;
+        ssl_session_cache       shared:SSL:50m;
+        ssl_session_tickets     off;
+
+        # Diffie-Hellman parameter for DHE ciphersuites, recommended 2048 bits
+        ssl_dhparam             certs/dhparam.pem;
+
+        # modern configuration. tweak to your needs.
+        ssl_protocols           TLSv1.2;
+        ssl_ecdh_curve      secp384r1;
+        ssl_ciphers 'ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256';
+        ssl_prefer_server_ciphers on;
+
+        # HSTS (ngx_http_headers_module is required) (15768000 seconds = 6 months)
+        add_header              Strict-Transport-Security max-age=15768000;
+        add_header              X-Frame-Options DENY;
+        add_header              X-Content-Type-Options nosniff;
+
+        # OCSP Stapling ---
+        # fetch OCSP records from URL in ssl_certificate and cache them
+        ssl_stapling            on;
+        ssl_stapling_verify     on;
+
+        ## verify chain of trust of OCSP response using Root CA and Intermediate certs
+        ssl_trusted_certificate certs/chain.pem;
+
+        resolver                8.8.8.8 8.8.4.4 valid=300s;
+
         location / {
           proxy_set_header Host $PORTUS_MACHINE_FQDN;
           proxy_set_header X-Forwarded-Proto https;
@@ -27,7 +63,7 @@ cat >/etc/nginx/conf.d/portus.conf <<_END_
           proxy_set_header Connection "upgrade";
           proxy_read_timeout 900s;
         }
-      }
+    }
 _END_
 
 cd /portus
@@ -39,7 +75,6 @@ if [ "$PORTUS_KEY_PATH" != "" -a "$PORTUS_MACHINE_FQDN" != "" -a ! -f "$PORTUS_K
     export ALTNAME=`hostname`
     export IPADDR=`ip addr list eth0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1|tail -1`
     openssl req -x509 -newkey rsa:2048 -keyout "$PORTUS_KEY_PATH" -out "$PORTUS_CRT_PATH" -days 3650 -nodes -subj "/CN=$PORTUS_MACHINE_FQDN" -extensions SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:registry,DNS:$PORTUS_MACHINE_FQDN,DNS:$ALTNAME,IP:$IPADDR,DNS:portus"))
-
 fi
 
 if [ "$PORTUS_MACHINE_FQDN" != "" ];then
